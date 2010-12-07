f5ff58d4467105daab1fbea1ed76c9660bbd1571
ptrf <petertersloevforsberg+github@gmail.com>
2010-12-07 14:40:01 +0100

    Temp commit: moving to work on palace

--- deckard/main.ml
+++ deckard/main.ml
@@ -1,1 +1,9 @@
-let x = 4+3
+open Common
+
+let read_ast_and_annote file =
+    print_endline "Parsing...";
+    let (pgm, _) = Parse_c.parse_print_error_heuristic file in
+    pgm
+
+
+let _ = read_ast_and_annote "./test.c"
---
+++ deckard/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int a = 2;
+
+    printf("%d\n", a);
+
+    return 0;
+
+};
